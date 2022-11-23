b25b96042fbcdfdc608e520c6fef2cc03c3f2ac0
Dong Pan <dong.pan@essexlg.com>
2022-11-23 22:02:47 +0800

    add class

--- 03. Puret/src/index.ts
+++ 03. Puret/src/index.ts
@@ -1,2 +1,13 @@
-console.log("typescript is here still.");
-console.log("typescript is amazing.");
+class User {
+  email: string;
+  name: string;
+  city: string = "xi'an";
+
+  constructor(email: string, name: string) {
+    this.email = email;
+    this.name = name;
+  }
+}
+
+const dp = new User("d.p@gmail.com", "dpan");
+dp.city = "sian";
