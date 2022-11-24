6ba5e88ba3fa40e14d9972c232bb68e2b8a358d0
Dong Pan <dong.pan@essexlg.com>
2022-11-24 12:08:59 +0800

    generic and type narrowing

---
+++ 03. Puret/src/detection.ts
@@ -0,0 +1,48 @@
+function detectType(val: number | string) {
+  if (typeof val === "string") {
+    return val.toLowerCase();
+  }
+  return val + 3;
+}
+
+function provideId(id: string | null) {
+  if (!id) {
+    console.log("Please provide ID");
+    return;
+  }
+  id.toLowerCase();
+}
+
+function printAll(strs: string | string[] | null) {
+  // !!!!!!!!!!!!!!!!
+  //  DON'T DO THIS!
+  //   KEEP READING
+  // !!!!!!!!!!!!!!!!
+  // empty string will be false and we will not handle empty string here
+  if (strs) {
+    if (typeof strs === "object") {
+      for (const s of strs) {
+        console.log(s);
+      }
+    } else if (typeof strs === "string") {
+      console.log(strs);
+    }
+  }
+}
+
+interface User {
+  name: string;
+  email: string;
+}
+interface Admin {
+  name: string;
+  email: string;
+  isAdmin: boolean;
+}
+
+function isAdmin(account: User | Admin): boolean {
+  if ("isAdmin" in account) {
+    return account.isAdmin;
+  }
+  return false;
+}
---
+++ 03. Puret/src/myGeneric.ts
@@ -0,0 +1,73 @@
+const score: Array<number> = [];
+const names: Array<string> = [];
+
+function identityOne(val: boolean | number): boolean | number {
+  return val;
+}
+
+function identityTwo(val: any): any {
+  return val;
+}
+
+function identityThree<Type>(val: Type): Type {
+  return val;
+}
+
+// identityThree(true);
+
+function identityFour<T>(val: T): T {
+  return val;
+}
+
+interface Bottle {
+  brand: string;
+  type: number;
+}
+
+identityFour<Bottle>({ brand: "bottle", type: 1 });
+
+function getSearchProducts<T>(products: T[]): T {
+  // do some database operations
+  const myIndex = 3;
+  return products[myIndex];
+}
+
+const getMoreSearchProducts = <T>(products: T[]): T => {
+  // do some database operations
+  const myIndex = 4;
+  return products[myIndex];
+};
+
+interface Database {
+  connection: string;
+  username: string;
+  password: string;
+}
+
+function anotherFunction<T, U extends Database>(valOne: T, valTwo: U): object {
+  return {
+    valOne,
+    valTwo,
+  };
+}
+
+anotherFunction(3, { connection: "aa", username: "dp", password: "123" });
+
+interface Quiz {
+  name: string;
+  type: string;
+}
+
+interface Course {
+  name: string;
+  author: string;
+  subject: string;
+}
+
+class Sellable<T> {
+  public cart: T[] = [];
+
+  addToCart(product: T) {
+    this.cart.push(product);
+  }
+}
