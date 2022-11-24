70bcc6905d8ba116e7cd0f2c14df06c490c50c6e
Dong Pan <dong.pan@essexlg.com>
2022-11-24 14:45:41 +0800

    finish typscript

--- 03. Puret/src/detection.ts
+++ 03. Puret/src/detection.ts
@@ -40,9 +40,79 @@ interface Admin {
   isAdmin: boolean;
 }
 
-function isAdmin(account: User | Admin): boolean {
+function isAdminAccount(account: User | Admin): boolean {
   if ("isAdmin" in account) {
     return account.isAdmin;
   }
   return false;
+}
+
+function logValue(x: Date | string) {
+  if (x instanceof Date) {
+    console.log(x.toUTCString());
+  } else {
+    console.log(x.toUpperCase());
+  }
+}
+
+type Fish = { swim: () => void };
+type Bird = { fly: () => void };
+
+function isFish(pet: Fish | Bird): pet is Fish {
+  return (pet as Fish).swim !== undefined;
+}
+
+function getFood(pet: Fish | Bird) {
+  if (isFish(pet)) {
+    pet;
+    return "fish food";
+  } else {
+    pet;
+    return "bird food";
+  }
+}
+
+interface Circle {
+  kind: "circle";
+  radius: number;
+}
+
+interface Square {
+  kind: "square";
+  side: number;
+}
+
+interface Rectangle {
+  kind: "rectangle";
+  length: number;
+  width: number;
+}
+
+type Shape = Circle | Square | Rectangle;
+
+function getTrueShape(shape: Shape) {
+  if (shape.kind === "circle") {
+    return Math.PI * shape.radius ** 2;
+  } else if (shape.kind === "square") {
+    return shape.side ** 2;
+  } else {
+    return shape.length * shape.width;
+  }
+}
+
+function getArea(shape: Shape) {
+  switch (shape.kind) {
+    case "circle":
+      return Math.PI * shape.radius ** 2;
+
+    case "square":
+      return shape.side ** 2;
+
+    case "rectangle":
+      return shape.length * shape.width;
+
+    default:
+      const _defaultforshape: never = shape;
+      return _defaultforshape;
+  }
 }
